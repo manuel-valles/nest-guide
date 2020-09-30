5672a345863c779a0284cd4a80f0bf4a24882cfc
Manuel Valles <manukempo@gmail.com>
2020-09-30 14:46:10 +0100

    Testing TasksService: updateTaskStatus

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -89,4 +89,19 @@ describe('TaskService', () => {
             expect(tasksService.deleteTask(1, mockUser)).rejects.toThrow(NotFoundException)
         })
     })
+
+    describe('updateTaskStatus', () => {
+        it('updates a task status', async () => {
+            const save = jest.fn().mockResolvedValue(true)
+            tasksService.getTaskById = jest.fn().mockResolvedValue({
+                status: TaskStatus.OPEN,
+                save
+            })
+            expect(tasksService.getTaskById).not.toHaveBeenCalled()
+            const result = await tasksService.updateTaskStatus(1, TaskStatus.DONE, mockUser)
+            expect(tasksService.getTaskById).toHaveBeenCalled()
+            expect(save).toHaveBeenCalled()
+            expect(result.status).toEqual(TaskStatus.DONE)
+        })
+    })
 })
